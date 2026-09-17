80cfc405926de788ec56ea8d50c95d2893622d4f
RonkleyTheRock <Ronkleytherock@gmail.com>
2026-09-17 13:15:34 +0100

    Add files via upload

---
+++ Main menu.py
@@ -0,0 +1,17 @@
+print("===================================")
+print("   SUPER AWESOME GAME FOR          ")
+print("        AWESOME PEOPLE             ")
+print("===================================")
+
+print()
+print("1. New Game")
+print("2. Exit")
+
+player_choice = input("Choose an option: ")
+
+if player_choice == "1":
+    print("Starting game...")
+elif player_choice == "2":
+    print("Thanks for playing!")
+else:
+    print("Invalid option. Please choose 1 or 2.")
